6029d5d256682ce3f060f62137df173f260aa660
patricktompkins <patrick.c.tompkins@gmail.com>
2018-01-29 10:45:53 -0500

    first commit

---
+++ IntrotoOOP/Cat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntrotoOOP
+{
+    class Cat
+    {
+        //I need STATES
+        //I need BEHAVIORS
+
+
+    }
+}
